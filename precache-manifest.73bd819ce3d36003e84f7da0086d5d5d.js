c58dc7ea224df7bc3628a0f55dad5192de1111e1
Naman Deol <namandeol555@gmail.com>
2024-05-16 13:40:01 +0530

    Updates

---
+++ precache-manifest.73bd819ce3d36003e84f7da0086d5d5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b514ab39792f95e87b4a558eaf7efd",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "4df57c1f4c9cfeea8d3e",
+    "url": "/my-portfolio/static/css/main.1f1f40a3.chunk.css"
+  },
+  {
+    "revision": "e6ec829cd51cae75a38e",
+    "url": "/my-portfolio/static/js/2.52752374.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/my-portfolio/static/js/2.52752374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ad4c0f9a4119b86ecf3",
+    "url": "/my-portfolio/static/js/3.6ce5ce29.chunk.js"
+  },
+  {
+    "revision": "4df57c1f4c9cfeea8d3e",
+    "url": "/my-portfolio/static/js/main.a43afdc8.chunk.js"
+  },
+  {
+    "revision": "b7e502fb8d61c18504a6",
+    "url": "/my-portfolio/static/js/runtime-main.3b33f927.js"
+  }
+]);
